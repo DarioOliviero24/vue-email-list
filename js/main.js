d546eb646ffae8759ac452a46b8d47929b1d57a4
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-09-03 11:18:31 +0200

    stampo le email in pagina

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 // console.log('Ciao');
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
   data() {
     return {
-      
+      emails: []
+    };
+  },
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+            const email = res.data.response;
+
+            console.log('Email' + i + ':', email);
+
+            this.emails.push(email);  
+        }); 
     }
+    
   }
-}).mount('#app')
+}).mount('#app');
